3c193c68853c314fcd0a32a57a7705f7209d7c92
Arshaq Kirmani <88348611+arshaqK@users.noreply.github.com>
2025-11-26 14:53:25 +0500

    Change app host from 0.0.0.0 to 127.0.0.1

--- app.py
+++ app.py
@@ -94,4 +94,4 @@ def convert():
 
 if __name__ == "__main__":
     # run on 0.0.0.0 for Colab tunneling
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
